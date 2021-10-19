5c01388758ebf11971cc8a5631b28ce632fa313a
Lautaro Blasco <lauti_blasco@hotmail.es>
2021-10-19 20:20:30 -0300

    actualizacion del primer entregable

--- primer_entregable.js
+++ primer_entregable.js
@@ -1,1 +1,7 @@
-{\rtf1}
+let numero_1 = Number(prompt("Ingresar un número: "))
+
+let numero_2 = Number(prompt("Ingresar un segundo número: "))
+
+let resultado = numero_1 ** numero_2
+
+alert("el primer numero elevado al segundo es: " + resultado)
