8ef9a2ef9ba84f896380dce39b3b137a2489a99f
Lautaro Blasco <lauti_blasco@hotmail.es>
2021-10-21 22:28:37 -0300

    Acomodé los archivos y subí el segundo entregable

---
+++ entregable-2/segundo_entregable.js
@@ -0,0 +1,14 @@
+let numero_secreto = Number(prompt("Ingresar número secreto (1,10) "))
+
+let palabra_secreta = prompt("Ingresar palabra secreta: ")
+
+if(numero_secreto == 4){
+    alert('Acertaste el número secreto!')
+}else{
+    alert('No acertaste el número secreto!')
+}
+if(palabra_secreta == 'Hola'){
+    alert('Acertaste la palabra secreta!')
+}else{
+    alert('No acertaste la palabra secreta!')
+}
